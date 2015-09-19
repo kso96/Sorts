6665f7b351537284ab7cc7b6b3ddd5bec76e6c16
KostyaVyrodov <kostya.vyrodov@mocoding.com>
2015-09-19 10:50:23 +0300

    Initial commit

---
+++ src/Sorts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+             
+            Console.Read();
+        }
+    }
+}
